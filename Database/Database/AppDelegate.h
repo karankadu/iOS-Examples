3ebf3ca0957ab49ef05c0aac7f8164e92b99e69c
karankadu <karan.p.kadu@gmail.com>
2016-11-09 01:22:07 +0530

    Database Demo

---
+++ Database/Database/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Database
+//
+//  Created by Felix-ITS 012 on 16/08/16.
+//  Copyright © 2016 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Database/Database/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Database
+//
+//  Created by Felix-ITS 012 on 16/08/16.
+//  Copyright © 2016 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
